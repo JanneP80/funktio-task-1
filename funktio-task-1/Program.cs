8808376d248a0ce35288f3b2ae224987ea51431d
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-20 11:04:45 +0200

    adding project

--- funktio-task-1/Program.cs
+++ funktio-task-1/Program.cs
@@ -8,8 +8,43 @@ namespace funktio_task_1
 {
     class Program
     {
+        
+        static string ScreenMessenger(int inputnumber)
+        {
+            string messagecounter = string.Empty; //string for returning stars "*"
+
+            if (inputnumber < 1) // is zero or negative -> not defined
+            {
+                messagecounter = string.Format("You typed number {0}, it is not defined.", inputnumber);
+            }
+            else
+
+            for (int i = 0; i < inputnumber; i++)
+            {
+                // Add more stars in the to be returned string.
+                messagecounter = messagecounter.Insert(0, "*"); 
+                
+            }
+            return messagecounter;
+        }
+
         static void Main(string[] args)
         {
+            int inputnumber = 0;
+            string message = string.Empty;
+            
+
+            Console.Write("This program prints as many stars as you want.\nPlease type amount: ");
+            bool result = int.TryParse(Console.ReadLine(), out inputnumber);
+            if (!result) // incorrect input
+            {
+                message = "Input is NOT a number.";
+            }
+            else
+
+            Console.Write(ScreenMessenger(inputnumber)); // Calls and prints
+
+            Console.ReadKey();
         }
     }
 }
