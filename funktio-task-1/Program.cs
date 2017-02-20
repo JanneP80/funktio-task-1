0ca469a913ca46855b0e89a3631c98c5a9a6c793
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-20 13:15:48 +0200

    update

--- funktio-task-1/Program.cs
+++ funktio-task-1/Program.cs
@@ -8,7 +8,25 @@ namespace funktio_task_1
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            int inputnumber = 0;
+            string message = string.Empty;
+
+
+            Console.Write("This program prints as many stars as you want.\nPlease type amount: ");
+            bool result = int.TryParse(Console.ReadLine(), out inputnumber);
+            if (!result) // incorrect input
+            {
+                Console.Write("Input is NOT a number.");
+            }
+            else
+            {
+                Console.Write(ScreenMessenger(inputnumber)); // Calls and prints
+            }
+            Console.ReadKey();
+        }
+
         static string ScreenMessenger(int inputnumber)
         {
             string messagecounter = string.Empty; //string for returning stars "*"
@@ -18,33 +36,16 @@ namespace funktio_task_1
                 messagecounter = string.Format("You typed number {0}, it is not defined.", inputnumber);
             }
             else
-
-            for (int i = 0; i < inputnumber; i++)
             {
-                // Add more stars in the to be returned string.
-                messagecounter = messagecounter.Insert(0, "*"); 
-                
+                for (int i = 0; i < inputnumber; i++)
+                {
+                    // Add more stars in the to be returned string.
+                    messagecounter = messagecounter.Insert(0, "*");
+                }
             }
             return messagecounter;
         }
 
-        static void Main(string[] args)
-        {
-            int inputnumber = 0;
-            string message = string.Empty;
-            
 
-            Console.Write("This program prints as many stars as you want.\nPlease type amount: ");
-            bool result = int.TryParse(Console.ReadLine(), out inputnumber);
-            if (!result) // incorrect input
-            {
-                message = "Input is NOT a number.";
-            }
-            else
-
-            Console.Write(ScreenMessenger(inputnumber)); // Calls and prints
-
-            Console.ReadKey();
-        }
     }
 }
